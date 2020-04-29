b58b6f7f76a51fc37db36d2427cd7aba6372a7d9
SilasDias <silasdjd@gmail.com>
2020-04-29 16:53:48 -0300

    Pasta JS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*var nome = "Silas Dias";
+alert("Bem Vindo " + nome);*/
+
+var frutas = [{nome:"Maça", cor:"Vermelho"}, {nome:"Uva", cor:"Roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+//var fruta = {nome:"Maça", cor:"Vermelho"}
+//console.log(fruta.nome);
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//console.log(lista.toString()[0]); //imprimi so o caracter da lista.
+//console.log(lista.join(" - ")); //Adiciona na string.
+//alert(lista[1])
